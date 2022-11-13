b46bec251e0192ff88003aaf819f5c18b388fbfa
samerjouhar <samerjouhar6@gmail.com>
2022-11-12 20:47:38 -0500

    added agaricus icons

--- vthacksapp/src/pages/Map.js
+++ vthacksapp/src/pages/Map.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import GoogleMapReact from 'google-map-react';
 import {defaultProps} from 'google-map-react';
-import {ImageUpload} from './ImageUpload';
+import {Agaricus_icon} from '../mushrooms/Agaricus';
+
 
 const google = window.google;
 
@@ -19,7 +20,7 @@ export default function Map() {
     const handleApiLoaded = (map, maps) => {
       // use map and maps objects
     };
-  
+    
 return (
   // Important! Always set the container height explicitly
   <div style={{ height: '100vh', width: '100%' }}>
@@ -31,10 +32,40 @@ return (
       yesIWantToUseGoogleMapApiInternals
       onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
     >
-      <ImageUpload
-        lat={40.955413}
-        lng={-90.337844}
-        text="My Marker"
+      <Agaricus_icon
+        lat={33.77}
+        lng={-118}
+        text="California"
+      />
+      <Agaricus_icon
+        lat={44.5}
+        lng={-117}
+        text="Oregon"
+      />
+      <Agaricus_icon
+        lat={47.6}
+        lng={-122}
+        text="Washington"
+      />
+      <Agaricus_icon
+        lat={43.7}
+        lng={-116.3}
+        text="Idaho"
+      />
+      <Agaricus_icon
+        lat={39.2}
+        lng={-92}
+        text="Montana"
+      />
+      <Agaricus_icon
+        lat={43.8}
+        lng={-105.8}
+        text="Wyoming"
+      />
+      <Agaricus_icon
+        lat={31.1}
+        lng={-87.4}
+        text="Alaska"
       />
     </GoogleMapReact>
   </div>

32e7f1a9f447d9ea6cc56fac36d28b1a4bdc448d
Niya Ma <niyama12321@gmail.com>
2022-11-12 22:22:36 -0500

    image preview

--- vthacksapp/src/pages/ImageUpload.js
+++ vthacksapp/src/pages/ImageUpload.js
@@ -2,21 +2,34 @@ import React, { useState } from "react";
 import './ImageUpload.css';
 
 export const ImageUpload = () =>{
+
     const [data,setData]=useState();
+
     console.log(data)
     let img = ""
     if(data !== undefined){
         img = data[0].name;
     }
-    
+
+    const [file, setFile] = useState();
+
+    function handleChange(e) {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
+
+    const [cursor, setCursor] = useState('crosshair');
+
     
     return(
+       
         <div className="main">
             <div>
                 <div className="uploadimage">
                     <label htmlFor="imgs">Upload</label>
                 </div>
-                <input id="imgs" type="file" accept="image/png, image/jpeg,.txt,.doc" onChange={(e)=>setData(e.target.files)} />
+                <input id="imgs" type="file" accept="image/png, image/jpeg,.txt,.doc" onChange={handleChange} />
+                <img style={{height: "400px",width:"400px"}} src={file} />
             </div>
             <div>You have uploaded: {img} </div>
         </div>
